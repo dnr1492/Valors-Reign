7e01d5207e494f4c98aed307b101ecb168426975
dnr1492 <dnr1492@gmail.com>
2025-04-09 11:54:04 +0900

    [Feat] Card 스크립트에 Enum 추가

--- Assets/Scripts/02_Card/Card.cs
+++ Assets/Scripts/02_Card/Card.cs
@@ -5,10 +5,13 @@ using UnityEngine.UI;
 
 public class Card : MonoBehaviour
 {
+    public enum State { None = -1, Front, Back }
+    //public enum CardType { None = -1, Character, Skill }
+
     [SerializeField] GameObject front, back;
     [SerializeField] Text skillCardName;
 
-    public void SetCardData(SkillCardData skillCardData)
+    public void SetCardData(SkillCardData skillCardData, State state = State.Back)
     {
         skillCardName.text = skillCardData.name;
     }
